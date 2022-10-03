f21eede9693dc4e3b5832b3818d20b706d05b62e
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-02 20:03:05 -0400

    Iterate through all valid records and print location

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -22,17 +22,21 @@ line_list = file_object.readlines()
 file_object.close()
 
 #Pretend we read one line of data from the file.
-lineString = line_list[20]
+for lineString in line_list:
 
-#Split the string into a list of data items.
-lineData = lineString.split()
+    #Check to see if the lineString is a data line.]
+    if lineString[0] == "#" or lineString[0] == "u":
+        continue
 
-#Extract items in list into variables.
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
-
-#Print the location of sara.
-print(f"Record {record_id} indicates Sara was seen at {obs_lat}N, {obs_lon}W on {obs_date}")
+    #Split the string into a list of data items.
+    lineData = lineString.split()
+    
+    #Extract items in list into variables.
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Print the location of sara.
+    print(f"Record {record_id} indicates Sara was seen at {obs_lat}N, {obs_lon}W on {obs_date}")
