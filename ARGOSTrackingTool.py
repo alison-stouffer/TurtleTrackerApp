ed82df3fd52d55b2aec7af5cfddd24113307dbbc
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-10-05 10:20:19 -0400

    Create error message for user input

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -58,6 +58,10 @@ for the_key, the_value in date_dict.items():
     #See if the date (the value) matches the user date.
     if the_value == user_date:
         matching_keys.append(the_key)
+        
+# Report whether no keys were found.
+if len(matching_keys) == 0:
+    print(f"Sara was not located on {user_date}")
 
 #Reveal locations for each key in matching_keys.
 for matching_key in matching_keys:
